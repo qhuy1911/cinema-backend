5d6609b4280a93804eca2f9bacca425646db528c
qhuy1911 <qhuy1911@gmail.com>
2022-06-03 22:08:52 +0700

    add movie controller

---
+++ src/main/java/com/example/cinema/controller/MovieController.java
@@ -0,0 +1,49 @@
+package com.example.cinema.controller;
+
+import com.example.cinema.exception.ResourceNotFoundException;
+import com.example.cinema.model.Movie;
+import com.example.cinema.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MovieController {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @GetMapping("/movies")
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = new ArrayList<>();
+        movieRepository.findAll().forEach(movies::add);
+        if (movies.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(movies, HttpStatus.OK);
+    }
+
+    @GetMapping("/movies/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable("id") long id) {
+        Movie movie = movieRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Movie with id = " + id));
+        return new ResponseEntity<>(movie, HttpStatus.OK);
+    }
+
+    @PostMapping("/movies")
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        Movie _movie = movieRepository.save(new Movie(
+                movie.getName(),
+                movie.getDescription(),
+                movie.getDirector(),
+                movie.getDuration(),
+                movie.getStartDate(),
+                movie.getTrailer()
+        ));
+        return new ResponseEntity<>(_movie, HttpStatus.CREATED);
+    }
+}
