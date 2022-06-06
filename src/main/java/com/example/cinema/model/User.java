631f8df4518d8a7c51a8eac711cd270b4c11fe52
qhuy1911 <qhuy1911@gmail.com>
2022-06-06 22:14:39 +0700

    add Role model and its relationship

--- src/main/java/com/example/cinema/model/User.java
+++ src/main/java/com/example/cinema/model/User.java
@@ -1,9 +1,18 @@
 package com.example.cinema.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name = "users")
+@Table( name = "users",
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = "username"),
+            @UniqueConstraint(columnNames = "email")
+        })
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,10 +21,20 @@ public class User {
     @Column(name = "fullName")
     private String fullName;
 
+    @NotBlank
+    @Column(name = "username")
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
     @Column(name = "password")
+    @Size(max = 120)
     private String password;
 
+    @NotBlank
     @Column(name = "email")
+    @Size(max = 50)
+    @Email
     private String email;
 
     @Column(name = "phone")
@@ -24,10 +43,17 @@ public class User {
     @Column(name = "address")
     private String address;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(	name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
     public User() {}
 
-    public User(String fullName, String password, String email, String phone, String address) {
+    public User(String fullName, String username, String password, String email, String phone, String address) {
         this.fullName = fullName;
+        this.username = username;
         this.password = password;
         this.email = email;
         this.phone = phone;
@@ -76,6 +102,22 @@ public class User {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 
     @Override
