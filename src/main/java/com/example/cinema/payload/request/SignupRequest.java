2258a7d81bea4a9a1dba90a737d886ab70b130e1
qhuy1911 <qhuy1911@gmail.com>
2022-06-07 21:31:26 +0700

    define payloads

--- src/main/java/com/example/cinema/payload/request/SignupRequest.java
+++ src/main/java/com/example/cinema/payload/request/SignupRequest.java
@@ -1,4 +1,88 @@
 package com.example.cinema.payload.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
 public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    @NotBlank
+    private String fullname;
+
+    @NotBlank
+    private String phone;
+
+    @NotBlank
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- src/main/java/com/example/cinema/payload/response/MessageReponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.cinema.payload.response;
-
-public class MessageReponse {
-}
