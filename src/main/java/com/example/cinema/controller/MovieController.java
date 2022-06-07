3d57fd7651036f75f075b3bb064547453af89846
qhuy1911 <qhuy1911@gmail.com>
2022-06-08 04:50:19 +0700

    update movie controller

--- src/main/java/com/example/cinema/controller/MovieController.java
+++ src/main/java/com/example/cinema/controller/MovieController.java
@@ -3,9 +3,11 @@ package com.example.cinema.controller;
 import com.example.cinema.exception.ResourceNotFoundException;
 import com.example.cinema.model.Movie;
 import com.example.cinema.repository.MovieRepository;
+import com.example.cinema.repository.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,9 @@ public class MovieController {
 
     @Autowired
     MovieRepository movieRepository;
+
+//    @Autowired
+//    ScheduleRepository scheduleRepository;
 
     @GetMapping("/movies")
     public ResponseEntity<List<Movie>> getAllMovies() {
@@ -47,5 +52,34 @@ public class MovieController {
                 movie.getTrailer()
         ));
         return new ResponseEntity<>(_movie, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/movies/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable("id") long id, @RequestBody Movie movie) {
+        Movie _movie = movieRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found MOVIE with id = " + id));
+        _movie.setName(movie.getName());
+        _movie.setDescription(movie.getDescription());
+        _movie.setDirector(movie.getDirector());
+        _movie.setDuration(movie.getDuration());
+        _movie.setStartDate(movie.getStartDate());
+        _movie.setTrailer(movie.getTrailer());
+
+        return new ResponseEntity<>(movieRepository.save(_movie), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/movies/{id}")
+    public ResponseEntity<Movie> deleteMovie(@PathVariable("id") long id) {
+//        scheduleRepository.deleteByMovieId(id);
+        movieRepository.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/movies")
+    public ResponseEntity<Movie> deleteAllMovies() {
+        movieRepository.deleteAll();
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
